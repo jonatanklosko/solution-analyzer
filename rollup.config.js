a9ea0680de06ef442be8ed5d006874a0360e73df
Jonatan Kłosko <jonatanklosko@gmail.com>
2018-08-27 23:17:50 +0200

    Initialize the repository. Include the core logic from the Reconstructions application.

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import pkg from './package.json';
+import babel from 'rollup-plugin-babel';
+
+export default {
+  input: 'src/solution-analyzer.js',
+  output: [
+		{ file: pkg.main, format: 'cjs' },
+		{ file: pkg.module, format: 'esm' },
+	],
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+      presets: [
+        ['@babel/preset-env', { modules: false }]
+      ],
+      plugins: ['@babel/plugin-proposal-object-rest-spread']
+    })
+  ]
+};
