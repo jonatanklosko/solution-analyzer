54b3b191a6f6b693df88c9c21edb67be302c7af7
Jonatan Kłosko <jonatanklosko@gmail.com>
2018-08-28 02:55:27 +0200

    Fix whitespaces in rollup.config.js.

--- rollup.config.js
+++ rollup.config.js
@@ -4,9 +4,9 @@ import babel from 'rollup-plugin-babel';
 export default {
   input: 'src/solution-analyzer.js',
   output: [
-		{ file: pkg.main, format: 'cjs' },
-		{ file: pkg.module, format: 'esm' },
-	],
+    { file: pkg.main, format: 'cjs' },
+    { file: pkg.module, format: 'esm' },
+  ],
   plugins: [
     babel({
       exclude: 'node_modules/**',
